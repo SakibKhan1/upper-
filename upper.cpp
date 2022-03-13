c38fac9afcc6daf5ee0f9217de2894e5855784bf
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-03-12 22:10:49 -0500

    Create upper.cpp

---
+++ upper.cpp
@@ -0,0 +1,31 @@
+/*
+Author: Sakib Khan 
+Course: CSCI-135
+Instructor: Tong Yee
+Assignment: Upper triangle
+Writes a program  
+that prints the top-right 
+half of a square, given 
+the side length.
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Input side length: ";
+    cin >> n;
+    cout << "Shape: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++){
+            if (j < i)
+                cout << " ";
+            else 
+                cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
